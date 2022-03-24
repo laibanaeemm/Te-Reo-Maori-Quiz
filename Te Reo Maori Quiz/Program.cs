aaf954fee4713b36b2fbcc7182bbe09219f5558e
ac149483 <ac149483@8CG8163KZW>
2022-03-24 15:13:40 +1300

    Add project files.

---
+++ Te Reo Maori Quiz/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+ * This quiz will help you learn Te Reo Maori
+ * First will be a question
+ * You will then be required to type your answer
+ * At the end of quiz, the computer will then show the answers to the questions
+ * 
+ * 
+ * 
+ */
+
+Console.WriteLine("Te Reo Maori Quiz!\n");
+
+Console.WriteLine("- This quiz will test you on how well you know Te Reo Maori.");
+Console.WriteLine("- If you are able to answer all 10 questions correctly, then you will pass.");
+Console.WriteLine("- Let's get started!\n\n");
+
+int answer;
+Console.WriteLine("How do you say brown in Te Reo. Is it: \n");
+Console.WriteLine("A. Whero");
+Console.WriteLine("B. Kowhai");
+Console.WriteLine("C. Karaka");
+Console.WriteLine("D. Waiporoporo ");
+answer = parauri;
+if (parauri)
+{ Console.WriteLine}
