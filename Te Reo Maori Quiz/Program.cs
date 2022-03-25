15ce3200ce0fb071d111753863fd19ce1b1e4fed
laibanaeemm <101070965+laibanaeemm@users.noreply.github.com>
2022-03-25 13:51:14 +1300

    Update Program.cs

--- Te Reo Maori Quiz/Program.cs
+++ Te Reo Maori Quiz/Program.cs
@@ -1,4 +1,4 @@
-﻿/* 
+/* 
  * This quiz will help you learn Te Reo Maori
  * First will be a question
  * You will then be required to type your answer
@@ -11,15 +11,56 @@
 Console.WriteLine("Te Reo Maori Quiz!\n");
 
 Console.WriteLine("- This quiz will test you on how well you know Te Reo Maori.");
-Console.WriteLine("- If you are able to answer all 10 questions correctly, then you will pass.");
-Console.WriteLine("- Let's get started!\n\n");
+Console.WriteLine("- If you are able to answer all 10 questions correctly, then you will win a star.");
+Console.WriteLine("- Let's get started!");
 
-int answer;
-Console.WriteLine("How do you say brown in Te Reo. Is it: \n");
-Console.WriteLine("A. Whero");
-Console.WriteLine("B. Kowhai");
-Console.WriteLine("C. Karaka");
-Console.WriteLine("D. Waiporoporo ");
-answer = parauri;
-if (parauri)
-{ Console.WriteLine}
+
+Console.WriteLine("Question 1: How do you say brown in Maori?");
+Console.ReadLine();
+if (paraone)
+{
+    Console.WriteLine("Correct!");
+}
+else
+{
+    Console.WriteLine("Wrong! Please try again");
+}
+
+Console.WriteLine("Question 2: What are the two ways you can say Black in Maori?");
+Console.ReadLine();
+if (mangu and pango){
+    Console.WriteLine("Correct!");
+} else
+{
+    Console.WriteLine("Wrong! Please try again");
+}
+
+Console.WriteLine("Question 3: How do you say purple in Maori");
+Console.ReadLine();
+if (waiporoporo)
+{
+    Console.WriteLine("Correct!");
+}
+else
+{
+    Console.WriteLine("Wrong! Please try again");
+}
+
+Console.WriteLine("Question 4: How do you say light green and dark green in Maori");
+Console.ReadLine();
+if (karerarera and karikiuri) {
+    Console.WriteLine("Correct!");
+} else
+{
+    Console.WriteLine("Wrong! Please try again");
+}
+
+Console.WriteLine("Question 5: How do you say pink in Maori");
+Console.ReadLine();
+if (mawhero)
+{
+    Console.WriteLine("Correct!");
+}
+else
+{
+    Console.WriteLine("Wrong! Please try again");
